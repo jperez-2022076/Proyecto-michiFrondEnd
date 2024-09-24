83ac0b7eab69eb3f6de6301f9c0da77f9a01acf7
jperez-2022076 <jperez-2022076@kinal.edu.gt>
2024-09-23 18:49:49 -0600

    Sisssshoy siintento2.1

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,7 +6,7 @@ import { HomePage } from './Pages/HomePages/HomePages';
 import Table from './componentes/Table';
 
 export const routes = [
-    { path: "/", element: <Navigate to="/login" /> },
+    { path: "*", element: <Navigate to="/login" /> },
     { path: "/login", element: <Login /> },
     { path: "/HomePage", element: <HomePage /> },
     { path: "/Usuarios", element: <Table /> }
