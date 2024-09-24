6bc5c699cdcf05bc6d66bfb345e137c75df8d5f6
jperez-2022076 <jperez-2022076@kinal.edu.gt>
2024-09-23 18:19:01 -0600

    Sisssshoy si

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import $ from 'jquery';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Importa Bootstrap JavaScript
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
+import 'datatables.net-bs4'; // Importa DataTables con Bootstrap 4
+import 'datatables.net-bs4/css/dataTables.bootstrap4.min.css';
 window.jQuery = $; // Esto hace que jQuery esté disponible globalmente
 
 
