49e84f5424fd472084f08b91428aa9f6bfa1cb7d
jperez-2022076 <jperez-2022076@kinal.edu.gt>
2024-09-23 18:35:37 -0600

    Sisssshoy si

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Asegúrate de usar './' si quieres rutas relativas para producción
   plugins: [react()],
 })
